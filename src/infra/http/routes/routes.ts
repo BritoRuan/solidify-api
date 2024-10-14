29cc84ea1eebf849144702c8bc7eb02e5d9251ee
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-14 12:28:54 -0300

    feat: added session route

--- src/infra/http/routes/routes.ts
+++ src/infra/http/routes/routes.ts
@@ -1,6 +1,8 @@
 import { register } from '@/module/controllers/registerUserController'
+import { authenticate } from '@/module/controllers/sessionUserController'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
