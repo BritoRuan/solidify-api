f0267b0ac368e72e443fdb301e9fdcbacc7b8779
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-14 12:30:01 -0300

    refactor: renamed some variables

--- src/module/useCases/users/index.spec.ts
+++ src/module/useCases/users/index.spec.ts
@@ -7,9 +7,9 @@ import { RegisterUseCase } from '.'
 describe('Register Use Case', () => {
   it('should be able to register.', async () => {
     const usersRepository = new UsersInMemoryRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const sut = new RegisterUseCase(usersRepository)
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: 'bolinha12345',
@@ -20,36 +20,36 @@ describe('Register Use Case', () => {
 
   it('should hash user password upon registration.', async () => {
     const usersRepository = new UsersInMemoryRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const sut = new RegisterUseCase(usersRepository)
 
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: 'bolinha12345',
     })
 
-    const isPasswordCorrectHashed = await compare(
+    const isPasswordCorrectlyHashed = await compare(
       'bolinha12345',
       user.passwordHash,
     )
 
-    expect(isPasswordCorrectHashed).toBe(true)
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 
   it('should not be able to register with same email twice', async () => {
     const usersRepository = new UsersInMemoryRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const sut = new RegisterUseCase(usersRepository)
 
     const email = 'johndoe@example.com'
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: 'John Doe',
       email,
       password: 'bolinha12345',
     })
 
     expect(() =>
-      registerUseCase.execute({
+      sut.execute({
         name: 'John Doe',
         email,
         password: 'bolinha12345',
