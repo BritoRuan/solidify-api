8ff198a1fcdf51aebd5075613507cdb9ad8f004e
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-16 12:13:32 -0300

    refactor: improving logic and code to check in use case

--- src/module/useCases/users/checkInUseCase/index.spec.ts
+++ src/module/useCases/users/checkInUseCase/index.spec.ts
@@ -1,22 +1,107 @@
 import { CheckInsMemoryRepository } from '@/module/repositories/contracts/CheckInsMemoryRepository/CheckInsMemoryRepository'
-import { describe, expect, it, beforeEach } from 'vitest'
+import { GymsInMemoryRepository } from '@/module/repositories/contracts/GymsInMemoryRepository/GymsInMemoryRepository'
+import { MaxNumberOfCheckInsError } from '@/module/errors/MaxNumberOfCheckInsError/MaxNumberOfCheckInsError'
+import { MaxDistanceError } from '@/module/errors/MaxDistanceError/MaxDistanceError'
+import { describe, expect, it, beforeEach, afterEach, vi } from 'vitest'
+import { Decimal } from '@prisma/client/runtime/library'
 import { CheckInUseCase } from './index'
 
-let usersRepository: CheckInsMemoryRepository
+let checksInsRepository: CheckInsMemoryRepository
+let gymsRepository: GymsInMemoryRepository
 let sut: CheckInUseCase
 
-describe('Register Use Case', () => {
-  beforeEach(() => {
-    usersRepository = new CheckInsMemoryRepository()
-    sut = new CheckInUseCase(usersRepository)
+describe('Check-in Use Case', () => {
+  beforeEach(async () => {
+    checksInsRepository = new CheckInsMemoryRepository()
+    gymsRepository = new GymsInMemoryRepository()
+    sut = new CheckInUseCase(checksInsRepository, gymsRepository)
+
+    await gymsRepository.create({
+      id: 'gym-01',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: -13.0097769,
+      longitude: -38.5317186,
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in.', async () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -13.0097769,
+      userLongitude: -38.5317186,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2024, 9, 15, 14, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -13.0097769,
+      userLongitude: -38.5317186,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+        userLatitude: -13.0097769,
+        userLongitude: -38.5317186,
+      }),
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2024, 9, 15, 14, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -13.0097769,
+      userLongitude: -38.5317186,
+    })
+
+    vi.setSystemTime(new Date(2024, 9, 26, 9, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -13.0097769,
+      userLongitude: -38.5317186,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym.', async () => {
+    gymsRepository.gyns.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(51.1143843),
+      longitude: new Decimal(3.9261365),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -13.0097769,
+        userLongitude: -38.5317186,
+      }),
+    ).rejects.toBeInstanceOf(MaxDistanceError)
   })
 })
--- src/module/useCases/users/checkInUseCase/index.ts
+++ src/module/useCases/users/checkInUseCase/index.ts
@@ -1,9 +1,16 @@
+import { MaxDistanceError } from '@/module/errors/MaxDistanceError/MaxDistanceError'
+import { MaxNumberOfCheckInsError } from '@/module/errors/MaxNumberOfCheckInsError/MaxNumberOfCheckInsError'
+import { ResourceNotFoundError } from '@/module/errors/ResourceNotFoundError/ResourceNotFoundError'
 import { ICheckInsRepository } from '@/module/repositories/contracts/CheckInsRepository'
+import { IGymsRepository } from '@/module/repositories/contracts/GymsRepository'
+import { getDistanceBetweenCoordinates } from '@/utils/getDistanceBetweenCoordiantes'
 import { CheckIn } from '@prisma/client'
 
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -11,12 +18,49 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRespository: ICheckInsRepository) {}
+  constructor(
+    private checkInsRespository: ICheckInsRepository,
+    private gymsRepository: IGymsRepository,
+  ) {}
 
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new MaxDistanceError()
+    }
+
+    const checkInOnSameDay = await this.checkInsRespository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDay) {
+      throw new MaxNumberOfCheckInsError()
+    }
+
     const checkIn = await this.checkInsRespository.create({
       gymId,
       userId,
