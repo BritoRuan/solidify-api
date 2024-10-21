fb8877e27483640986a8a28bba607250b5ecc2ab
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-21 14:03:04 -0300

    feat: added new settings and libraries

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import fastify from 'fastify'
 import { appRoutes } from './infra/http/routes/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 app.register(appRoutes)
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
