2d499a3c97919a93dad44a01f9363c4d134b761c
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-21 14:07:12 -0300

    feat: added new route

--- src/infra/http/routes/routes.ts
+++ src/infra/http/routes/routes.ts
@@ -1,8 +1,13 @@
-import { register } from '@/module/controllers/registerUserController'
-import { authenticate } from '@/module/controllers/sessionUserController'
 import { FastifyInstance } from 'fastify'
+import { authenticate } from '../controllers/sessionUserController'
+import { profile } from '../controllers/profileUserController'
+import { register } from '../controllers/registerUserController'
+import { verifyJWT } from '../middlewares/verifyJwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
