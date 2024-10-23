6ee48ef1e17b3e1c2021494cf5c17659d8da4e23
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-22 22:15:12 -0300

    chore: added new config to routes users and app

--- src/infra/http/controllers/users/routesUsers.ts
+++ src/infra/http/controllers/users/routesUsers.ts
@@ -3,10 +3,13 @@ import { verifyJWT } from '@/infra/http/middlewares/verifyJwt'
 import { authenticate } from './sessionUserController'
 import { register } from './registerUserController'
 import { profile } from './profileUserController'
+import { refresh } from './refreshUserController'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
